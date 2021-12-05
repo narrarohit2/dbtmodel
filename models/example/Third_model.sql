eb7601bf3fc36fb1bc6595def0a5f7fd26384296
Rohit <rohitnarra2@gmail.com>
2021-12-05 16:55:30 +0530

    model add

---
+++ models/example/Third_model.sql
@@ -0,0 +1,11 @@
+{ config(materialized='table') }}
+
+with source_data as (
+
+    select * from FIVETRAN_INTERVIEW_DB.GOOGLE_SHEETS.COVID_19_INDONESIA_ROHIT_NARRA
+
+)
+
+select *
+from source_data
+
