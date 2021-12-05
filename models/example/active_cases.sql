b1f2b95974f035a0fc474b40e7ab9905f5c1868f
Rohit <rohitnarra2@gmail.com>
2021-12-05 18:50:41 +0530

    model add

--- models/example/active_cases.sql
+++ models/example/active_cases.sql
@@ -2,7 +2,7 @@
 
 with source_data as (
 
-   select DATE,max(TOTAL_ACTIVE_CASES) as max_cases_per_day from MAIN_TABLE group by DATE;
+   select DATE,max(TOTAL_ACTIVE_CASES) as max_cases_per_day from MAIN_TABLE group by DATE
 
 )
 
