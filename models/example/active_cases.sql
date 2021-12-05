fb94b3b84ab9d00fd0671dc2b7ed75f42a9828e5
Rohit <rohitnarra2@gmail.com>
2021-12-05 18:48:56 +0530

    model add

---
+++ models/example/active_cases.sql
@@ -0,0 +1,9 @@
+{{ config(materialized='table') }}
+
+with source_data as (
+
+   select DATE,max(TOTAL_ACTIVE_CASES) as max_cases_per_day from MAIN_TABLE group by DATE;
+
+)
+
+select * from source_data
